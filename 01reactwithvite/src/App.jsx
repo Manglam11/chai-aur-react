59c73d262d66ef1952c40333dbc827da0ac63f16
MANGLAM DUBEY <manglamdubey11@gmail.com>
2024-09-12 05:47:59 +0530

    ban ra h project

--- 01reactwithvite/src/App.jsx
+++ 01reactwithvite/src/App.jsx
@@ -2,12 +2,12 @@ import Chai from './chai.jsx'
 
 function App() {
 
-
+  const username = "Manglam"
   return (
     // <h1>Hover effect</h1>
     <>
       <Chai />
-      <h2>Namaste from this side.</h2>
+      <h2>Namaste from this side. This is {username}</h2>
       <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
     </>
   )
--- 01reactwithvite/src/main.jsx
+++ 01reactwithvite/src/main.jsx
@@ -1,10 +1,41 @@
 import { StrictMode } from 'react'
+import React from "react";
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp() {
+  return (
+    <div>
+      <h2>Custom App!</h2>
+    </div>
+  )
+}
+// const ReactElement = {
+//   type: "a",
+//   props: {
+//     href: "https://google.com",
+//     target: "_blank",
+//   },
+//   children: "Click me to visit google",
+// };
+
+const anotherElement = (
+  <a href="https://google.com" target='_blank'>Visit Google</a>
+)
+
+const reactElement = React.createElement(
+  'a',
+  {
+    href: "https://google.com",
+    target: "_blank",
+  },
+  "google m anne k liye dhakka de!"
+)
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+
+  // <MyApp />
+  // anotherElement
+  // reactElement
+  <App />
 )
