32a7065dacd44775589b8626144caca7297a74be
MANGLAM DUBEY <manglamdubey11@gmail.com>
2024-09-12 05:48:28 +0530

    ban ra h project

--- customReact/customreact.js
+++ customReact/customreact.js
@@ -1,6 +1,22 @@
+function customRender(reactElement, container) {
+  //   const domElement = document.createElement(reactElement.type);
+  //   domElement.innerHTML = reactElement.children;
+  //   domElement.setAttribute("href", reactElement.porps.href);
+  //   domElement.setAttribute("target", reactElement.porps.target);
+  //   container.appendChild(domElement);
+
+  const domElement = document.createElement(reactElement.type);
+  domElement.innerHTML = reactElement.children;
+  for (const prop in reactElement.props) {
+    if (prop === "child") continue;
+    domElement.setAttribute(prop, reactElement.props[prop]);
+    container.appendChild(domElement);
+  }
+}
+
 const reactElement = {
   type: "a",
-  porps: {
+  props: {
     href: "https://google.com",
     target: "_blank",
   },
@@ -8,3 +24,4 @@ const reactElement = {
 };
 
 const mainContainer = document.getElementById("root");
+customRender(reactElement, mainContainer);
