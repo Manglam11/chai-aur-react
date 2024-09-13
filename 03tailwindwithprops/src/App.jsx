207b0b12eeb888c0a87da32fbc7a0259e33e3d53
MANGLAM DUBEY <manglamdubey11@gmail.com>
2024-09-13 21:21:10 +0530

    chal chuke mitwa!

--- 03tailwindwithprops/src/App.jsx
+++ 03tailwindwithprops/src/App.jsx
@@ -2,31 +2,20 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './components/Card'
 
 function App() {
+  let myObj = {
+    isSkilled: true,
+    probability: 2000
+  }
+  let myArr = [1, 2, 3, 4, 5, 6]
   return (
     <>
-      <h1 className='bg-green-400 text-black rounded-xl p-4'>Namaste Tailwind</h1>
-      <figure class="bg-slate-100 rounded-xl p-8 dark:bg-slate-800">
-        <img class="w-24 h-24 rounded-full" src="https://www.pexels.com/photo/gray-owl-perched-on-a-tree-stamp-206676/" alt="" width="384" height="512" />
-        <div class="pt-6 space-y-4">
-          <blockquote>
-            <p class="text-lg">
-              “Tailwind CSS is the only framework that I've seen scale
-              on large teams. It’s easy to customize, adapts to any design,
-              and the build size is tiny.”
-            </p>
-          </blockquote>
-          <figcaption>
-            <div>
-              Sarah Dayan
-            </div>
-            <div>
-              Staff Engineer, Algolia
-            </div>
-          </figcaption>
-        </div>
-      </figure></>
+      <h1 className='bg-green-400 text-black rounded-xl p-4 mb-4'>Namaste Tailwind</h1>
+      <Card username='Manglam' btnText="click me" />
+      <Card username='Dubey' />
+    </>
   )
 }
 
