d575d53181e288979169a9e11a98c054a4cff76c
MANGLAM DUBEY <manglamdubey11@gmail.com>
2024-09-13 20:42:18 +0530

    chalo chale mitewa!

--- 03tailwindwithprops/src/App.jsx
+++ 03tailwindwithprops/src/App.jsx
@@ -4,12 +4,29 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-
-
   return (
     <>
-      <h1 className='bg-green-400'>Namaste Tailwdid</h1>
-    </>
+      <h1 className='bg-green-400 text-black rounded-xl p-4'>Namaste Tailwind</h1>
+      <figure class="bg-slate-100 rounded-xl p-8 dark:bg-slate-800">
+        <img class="w-24 h-24 rounded-full" src="https://www.pexels.com/photo/gray-owl-perched-on-a-tree-stamp-206676/" alt="" width="384" height="512" />
+        <div class="pt-6 space-y-4">
+          <blockquote>
+            <p class="text-lg">
+              “Tailwind CSS is the only framework that I've seen scale
+              on large teams. It’s easy to customize, adapts to any design,
+              and the build size is tiny.”
+            </p>
+          </blockquote>
+          <figcaption>
+            <div>
+              Sarah Dayan
+            </div>
+            <div>
+              Staff Engineer, Algolia
+            </div>
+          </figcaption>
+        </div>
+      </figure></>
   )
 }
 
